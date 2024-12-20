253bfdd33dd3d9aab9a39e822e4ddee6e3402fb0
Aden Mei <amei16@cps.edu>
2024-12-20 14:13:01 -0600

    fin

--- part2-training-testing-data/part2_writeup.md
+++ part2-training-testing-data/part2_writeup.md
@@ -5,9 +5,10 @@ After completing `a6_part2.py` answer the following questions
 ## Questions to answer
 
 1. What makes this model more effective than the model you created in the previous lesson?
-
+It creates predicted data points so it can more accurately create a best line of fit.
 
 2. What does the R squared coefficient tell you about the model?
-
+Somewhat similar to part 1 where it tests how well the model fits the training data that we made.
 
 3. Would you say that your model is accurate? What evidence supports your conclusion? Consider the meaning of the predicted and actual values in the context of the chart below from the American Heart Association’s website on understanding blood pressure.
+I would say that the model is now more accurate than the one in part 1 because using the predictive modeling we can better predict a line of best fit.
--- part3-multivariable-linear-regression/a6_part3.py
+++ part3-multivariable-linear-regression/a6_part3.py
@@ -9,14 +9,27 @@ x = data[["miles","age"]].values
 y = data["Price"].values
 
 #split the data into training and testing data
-
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size = .2)
 #create linear regression model
-
+model = LinearRegression().fit(xtrain, ytrain)
 #Find and print the coefficients, intercept, and r squared values. 
 #Each should be rounded to two decimal places. 
-
+coef = round(float(model.coef_[0]), 2)
+intercept = round(float(model.intercept_), 2)
+r_squared = model.score(xtrain, ytrain)
+print("Model's Linear Equation: y=",coef, "x+", intercept)
+print("R Squared value:", r_squared)
 
 #Loop through the data and print out the predicted prices and the 
 #actual prices
+predict = model.predict(xtest)
+predict = np.around(predict, 2)
+
 print("***************")
 print("Testing Results")
+
+for index in range(len(xtest)):
+    actual = ytest[index]
+    predicted_y = predict[index]
+    x_coord = xtest[index]
+    print("x value:", float(x_coord[0]), "Predicted y value:", predicted_y, "Actual y value:", actual)
